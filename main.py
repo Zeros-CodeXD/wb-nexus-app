f3765dcbc7c07497623b5c3aac299a23df3ed07e
Zeros-CodeXD <87669132+Zeros-CodeXD@users.noreply.github.com>
2026-02-26 17:07:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,63 @@
 import flet as ft
-import time
 
 # --- 1. THE COMPLETE MASTER DATABASE (EMBEDDED) ---
 DATABASE = {
   # --- BOOKS ---
   "books_class_10": [
-    {"title": "Sahitya Sanchayan (Bengali)", "link": "https://drive.google.com/file/d/1iQAADSemG8pJCG2phDGwGEdrpNGG1_9m/view?usp=drive_link"},
-    {"title": "Bliss (English)", "link": "https://drive.google.com/file/d/1J8ACc0y2ftQ_wSjEqx-C0sA_d8ZjImkx/view?usp=drive_link"},
-    {"title": "Ganit Prakash (Math)", "link": "https://drive.google.com/file/d/11TITkwOSGAFYSz015YStiaxUOUExTkfa/view?usp=drive_link"},
-    {"title": "Koni (Rapid Reader)", "link": "https://drive.google.com/file/d/1jJ-YJIkSyYFG7bSt9Ui9RLfZDWApLKDp/view?usp=drive_link"},
-    {"title": "Physical Science", "link": "https://drive.google.com/file/d/YOUR_LINK_HERE"},
-    {"title": "Life Science", "link": "https://drive.google.com/file/d/YOUR_LINK_HERE"},
-    {"title": "History & Geography", "link": "https://drive.google.com/file/d/YOUR_LINK_HERE"}
+    {"title": "Sahitya Sanchayan (Bengali)", "link": "https://drive.google.com/file/d/1iQAADSemG8pJCG2phDGwGEdrpNGG1_9m/view"},
+    {"title": "Bliss (English)", "link": "https://drive.google.com/file/d/1J8ACc0y2ftQ_wSjEqx-C0sA_d8ZjImkx/view"},
+    {"title": "Ganit Prakash (Math)", "link": "https://drive.google.com/file/d/11TITkwOSGAFYSz015YStiaxUOUExTkfa/view"},
+    {"title": "Koni (Rapid Reader)", "link": "https://drive.google.com/file/d/1jJ-YJIkSyYFG7bSt9Ui9RLfZDWApLKDp/view"},
+    {"title": "Physical Science (Bhouto Bigyan)", "link": "https://drive.google.com/file/d/YOUR_LINK_HERE"},
+    {"title": "Life Science (Jiban Bigyan)", "link": "https://drive.google.com/file/d/YOUR_LINK_HERE"},
+    {"title": "History (Itihas)", "link": "https://drive.google.com/file/d/YOUR_LINK_HERE"},
+    {"title": "Geography (Bhugol)", "link": "https://drive.google.com/file/d/YOUR_LINK_HERE"}
   ],
   "books_class_9": [
-    {"title": "Sahitya Sanchayan (Class 9)", "link": "https://drive.google.com/file/d/1W1iXt6ba7cKGXK06wU0PdDAP9yq_BNdI/view?usp=drive_link"},
-    {"title": "Bliss (Class 9 English)", "link": "https://drive.google.com/file/d/1MJfjJt_9UrG04iMAanWz2izBx_RaBsgc/view?usp=drive_link"},
-    {"title": "Ganit Prakash (Class 9)", "link": "https://drive.google.com/file/d/1bLVwDsYfENQPta5O6w6DQMOvFURorsJj/view?usp=drive_link"},
-    {"title": "Aam Aatir Bhepu", "link": "https://drive.google.com/file/d/1LdJle7dtqQnfdds_0ENfj3a6KmWACqu-/view?usp=drive_link"}
+    {"title": "Sahitya Sanchayan (Class 9)", "link": "https://drive.google.com/file/d/1W1iXt6ba7cKGXK06wU0PdDAP9yq_BNdI/view"},
+    {"title": "Bliss (Class 9 English)", "link": "https://drive.google.com/file/d/1MJfjJt_9UrG04iMAanWz2izBx_RaBsgc/view"},
+    {"title": "Ganit Prakash (Class 9)", "link": "https://drive.google.com/file/d/1bLVwDsYfENQPta5O6w6DQMOvFURorsJj/view"},
+    {"title": "Aam Aatir Bhepu", "link": "https://drive.google.com/file/d/1LdJle7dtqQnfdds_0ENfj3a6KmWACqu-/view"},
+    {"title": "Physical Science (Class 9)", "link": "https://drive.google.com/file/d/YOUR_LINK_HERE"},
+    {"title": "Life Science (Class 9)", "link": "https://drive.google.com/file/d/YOUR_LINK_HERE"}
   ],
   "books_class_8": [
-    {"title": "Poribesh O Bigyan (Science)", "link": "https://drive.google.com/file/d/18BUfpM6iwruuXPcOyNTpYpIsifrwypPN/view?usp=sharing"},
-    {"title": "Blossoms (English)", "link": "https://drive.google.com/file/d/1ETK2c1uJ802-hOiy0AsYHcmt8imEv_Fw/view?usp=drive_link"},
-    {"title": "Sahitya Mela (Bengali)", "link": "https://drive.google.com/file/d/197KM-v4IG5Zq7SwebNJ1kXv4OuSCwVDF/view?usp=drive_link"},
-    {"title": "Bhasha Charcha (Grammar)", "link": "https://drive.google.com/file/d/1oVPMJrl6iYGF_tYeIzCYpwRBpxd9vLg3/view?usp=drive_link"},
-    {"title": "Pather Panchali", "link": "https://drive.google.com/file/d/1q_cJl8L5XLc96NZA2Cm5x9P_D52jIwNj/view?usp=drive_link"},
-    {"title": "Atit O Aitihya (History)", "link": "https://drive.google.com/file/d/1z9yFpVeblEDkqlOueifS5bOoL-It_y6t/view?usp=drive_link"},
-    {"title": "Amader Prithibi (Geo)", "link": "https://drive.google.com/file/d/117Pahy31xCyuCGBa_7y_G7OKIE01N4Ch/view?usp=drive_link"}
+    {"title": "Poribesh O Bigyan (Science)", "link": "https://drive.google.com/file/d/18BUfpM6iwruuXPcOyNTpYpIsifrwypPN/view"},
+    {"title": "Blossoms (English)", "link": "https://drive.google.com/file/d/1ETK2c1uJ802-hOiy0AsYHcmt8imEv_Fw/view"},
+    {"title": "Sahitya Mela (Bengali)", "link": "https://drive.google.com/file/d/197KM-v4IG5Zq7SwebNJ1kXv4OuSCwVDF/view"},
+    {"title": "Bhasha Charcha (Grammar)", "link": "https://drive.google.com/file/d/1oVPMJrl6iYGF_tYeIzCYpwRBpxd9vLg3/view"},
+    {"title": "Pather Panchali", "link": "https://drive.google.com/file/d/1q_cJl8L5XLc96NZA2Cm5x9P_D52jIwNj/view"},
+    {"title": "Atit O Aitihya (History)", "link": "https://drive.google.com/file/d/1z9yFpVeblEDkqlOueifS5bOoL-It_y6t/view"},
+    {"title": "Amader Prithibi (Geo)", "link": "https://drive.google.com/file/d/117Pahy31xCyuCGBa_7y_G7OKIE01N4Ch/view"}
   ],
   
   # --- PAPERS ---
   "papers_2024": [
-    {"title": "2024 Bengali Paper", "link": "https://drive.google.com/file/d/1cIMbMTMDD0uam_Dpgbw_Pwq3wxaMNoOI/view?usp=drive_link"},
-    {"title": "2024 English Paper", "link": "https://drive.google.com/file/d/1Jy1Mje6v1VExMIs828UMIekf8m7NyXRu/view?usp=drive_link"},
-    {"title": "2024 Math Paper", "link": "https://drive.google.com/file/d/1K21V1xGZEAFGXsGjojFzTo74KqpDFmC-/view?usp=drive_link"},
-    {"title": "2024 Physical Science", "link": "https://drive.google.com/file/d/1isrdJmWektP7UNi4heeYF52jcuzHHFqF/view?usp=drive_link"},
-    {"title": "2024 Life Science", "link": "https://drive.google.com/file/d/10uMrtr3cOujv_e07XkaliaEx16Gv9XGf/view?usp=drive_link"},
-    {"title": "2024 History", "link": "https://drive.google.com/file/d/1i1C-plnjdPTpHN551J3xHlcxFzkHRQNE/view?usp=drive_link"},
-    {"title": "2024 Geography", "link": "https://drive.google.com/file/d/1M3oyhEVcA_XW6_-X5L4l1eAhEJTPDh41/view?usp=drive_link"}
+    {"title": "2024 Bengali Paper", "link": "https://drive.google.com/file/d/1cIMbMTMDD0uam_Dpgbw_Pwq3wxaMNoOI/view"},
+    {"title": "2024 English Paper", "link": "https://drive.google.com/file/d/1Jy1Mje6v1VExMIs828UMIekf8m7NyXRu/view"},
+    {"title": "2024 Math Paper", "link": "https://drive.google.com/file/d/1K21V1xGZEAFGXsGjojFzTo74KqpDFmC-/view"},
+    {"title": "2024 Physical Science", "link": "https://drive.google.com/file/d/1isrdJmWektP7UNi4heeYF52jcuzHHFqF/view"},
+    {"title": "2024 Life Science", "link": "https://drive.google.com/file/d/10uMrtr3cOujv_e07XkaliaEx16Gv9XGf/view"},
+    {"title": "2024 History", "link": "https://drive.google.com/file/d/1i1C-plnjdPTpHN551J3xHlcxFzkHRQNE/view"},
+    {"title": "2024 Geography", "link": "https://drive.google.com/file/d/1M3oyhEVcA_XW6_-X5L4l1eAhEJTPDh41/view"}
   ],
   "papers_2023": [
-    {"title": "2023 Bengali Paper", "link": "https://drive.google.com/file/d/1Eijq8S0kYZG6pZK3cwoO5pcNcIxslH_W/view?usp=drive_link"},
-    {"title": "2023 English Paper", "link": "https://drive.google.com/file/d/1XnGDf2ekn4ljc5LP4_kpRPFXkXrhDuHs/view?usp=drive_link"},
-    {"title": "2023 Math Paper", "link": "https://drive.google.com/file/d/1q8ev-LLr6Ry7jwIWY3M3n3oO3V2rehx-/view?usp=drive_link"},
-    {"title": "2023 Physical Science", "link": "https://drive.google.com/file/d/1GvH9M-sEWNIX9vDM2GOKmFx1OZyCHHlO/view?usp=drive_link"},
-    {"title": "2023 Life Science", "link": "https://drive.google.com/file/d/1LywHj2UH2mc2qrJD4KPPKvYbsfwAOTYx/view?usp=drive_link"},
-    {"title": "2023 History", "link": "https://drive.google.com/file/d/1LSiZV1yY4QtUPq8Xu8HWFxCDOpScnO7y/view?usp=drive_link"},
-    {"title": "2023 Geography", "link": "https://drive.google.com/file/d/1shP3OooAaWdeoM2oAnxWR92TEw5uU3QF/view?usp=drive_link"}
+    {"title": "2023 Bengali Paper", "link": "https://drive.google.com/file/d/1Eijq8S0kYZG6pZK3cwoO5pcNcIxslH_W/view"},
+    {"title": "2023 English Paper", "link": "https://drive.google.com/file/d/1XnGDf2ekn4ljc5LP4_kpRPFXkXrhDuHs/view"},
+    {"title": "2023 Math Paper", "link": "https://drive.google.com/file/d/1q8ev-LLr6Ry7jwIWY3M3n3oO3V2rehx-/view"},
+    {"title": "2023 Physical Science", "link": "https://drive.google.com/file/d/1GvH9M-sEWNIX9vDM2GOKmFx1OZyCHHlO/view"},
+    {"title": "2023 Life Science", "link": "https://drive.google.com/file/d/1LywHj2UH2mc2qrJD4KPPKvYbsfwAOTYx/view"},
+    {"title": "2023 History", "link": "https://drive.google.com/file/d/1LSiZV1yY4QtUPq8Xu8HWFxCDOpScnO7y/view"},
+    {"title": "2023 Geography", "link": "https://drive.google.com/file/d/1shP3OooAaWdeoM2oAnxWR92TEw5uU3QF/view"}
   ],
   "papers_2022": [
-    {"title": "2022 Bengali Paper", "link": "https://drive.google.com/file/d/1qiNdm9CJYefLZ-wF-qDLCQnMZNbR7ebU/view?usp=drive_link"},
-    {"title": "2022 English Paper", "link": "https://drive.google.com/file/d/1vuK6W9evHONNvHmk2nUOxvIYw6BdCCtr/view?usp=drive_link"},
-    {"title": "2022 Math Paper", "link": "https://drive.google.com/file/d/1XDwjyeTHDkRnXjJatqMjG-ZtFNEb3hBW/view?usp=drive_link"},
-    {"title": "2022 Physical Science", "link": "https://drive.google.com/file/d/1sYrcwl5gLVCNEFEcrR4QypBsBe78IQ9g/view?usp=drive_link"},
-    {"title": "2022 Life Science", "link": "https://drive.google.com/file/d/1zNaUAAEklNCKYdv_dRny_gyfotg5eGOM/view?usp=drive_link"},
-    {"title": "2022 History", "link": "https://drive.google.com/file/d/1ZW4c3u6-G9gPXbMxAO1MQk6-_kh9r05g/view?usp=drive_link"},
-    {"title": "2022 Geography", "link": "https://drive.google.com/file/d/19QWjSKary4IXrv9Bwa0oyADDTXmc3dPA/view?usp=drive_link"}
+    {"title": "2022 Bengali Paper", "link": "https://drive.google.com/file/d/1qiNdm9CJYefLZ-wF-qDLCQnMZNbR7ebU/view"},
+    {"title": "2022 English Paper", "link": "https://drive.google.com/file/d/1vuK6W9evHONNvHmk2nUOxvIYw6BdCCtr/view"},
+    {"title": "2022 Math Paper", "link": "https://drive.google.com/file/d/1XDwjyeTHDkRnXjJatqMjG-ZtFNEb3hBW/view"},
+    {"title": "2022 Physical Science", "link": "https://drive.google.com/file/d/1sYrcwl5gLVCNEFEcrR4QypBsBe78IQ9g/view"},
+    {"title": "2022 Life Science", "link": "https://drive.google.com/file/d/1zNaUAAEklNCKYdv_dRny_gyfotg5eGOM/view"},
+    {"title": "2022 History", "link": "https://drive.google.com/file/d/1ZW4c3u6-G9gPXbMxAO1MQk6-_kh9r05g/view"},
+    {"title": "2022 Geography", "link": "https://drive.google.com/file/d/19QWjSKary4IXrv9Bwa0oyADDTXmc3dPA/view"}
   ],
 
   # --- COLLEGES (FULL LIST) ---
@@ -92,6 +94,7 @@ def main(page: ft.Page):
     page.theme_mode = ft.ThemeMode.DARK
     page.padding = 0
     page.spacing = 0
+    # Material 3 Theme with Cyan Seed
     page.theme = ft.Theme(color_scheme_seed="cyan", use_material3=True)
 
     # --- 2. LINK HANDLER ---
@@ -120,12 +123,16 @@ def main(page: ft.Page):
 
     def create_college_card(item):
         seats = item['seats']
+        # Logic for Status Colors
         if seats == 0:
-            status_col, status_text = "red", "CLOSED"
+            status_col = "red"
+            status_text = "CLOSED"
         elif seats < 10:
-            status_col, status_text = "orange", f"FAST: {seats} LEFT"
+            status_col = "orange"
+            status_text = f"FAST: {seats} LEFT"
         else:
-            status_col, status_text = "green", f"OPEN: {seats} SEATS"
+            status_col = "green"
+            status_text = f"OPEN: {seats} SEATS"
 
         return ft.Container(
             content=ft.Column([
@@ -142,7 +149,7 @@ def main(page: ft.Page):
                 ft.Divider(height=10, color="#333"),
                 ft.Row([
                     ft.Text(f"Cutoff: {item['cutoff']}", size=12, color="grey"),
-                    ft.ElevatedButton("Apply", height=30, 
+                    ft.ElevatedButton("Official Site", height=30, 
                                       style=ft.ButtonStyle(bgcolor="#222", color="white", side=ft.BorderSide(1, "cyan")), 
                                       data=item['link'], on_click=handle_link)
                 ], alignment="spaceBetween")
@@ -150,51 +157,39 @@ def main(page: ft.Page):
             padding=15, margin=ft.margin.only(bottom=10), bgcolor="#1a1a1a", border_radius=15, border=ft.border.all(1, "#333")
         )
 
-    # --- 4. PRE-LOADING SCREEN (Prevents Black Screen) ---
-    loader = ft.Container(
-        content=ft.Column([
-            ft.ProgressRing(color="cyan", stroke_width=5),
-            ft.Container(height=20),
-            ft.Text("WB NEXUS", size=30, weight="bold", color="cyan"),
-            ft.Text("Loading Database...", color="grey")
-        ], alignment="center", horizontal_alignment="center"),
-        alignment=ft.alignment.center, expand=True, bgcolor="#0a0a0a"
-    )
-    page.add(loader)
-    page.update()
+    # --- 4. VIEW LOGIC (Direct Load) ---
     
-    # Simulate Safe Load
-    time.sleep(1.5)
-
-    # --- 5. BUILD CONTENT ---
+    # We create the content immediately. No functions, no delays.
     
-    # Books View
+    # -- BOOKS CONTENT --
     books_col = ft.Column(scroll="auto", padding=15)
     for k in ["books_class_10", "books_class_9", "books_class_8"]:
         if k in DATABASE:
             books_col.controls.append(ft.Text(k.replace("books_", "CLASS ").upper(), color="orange", weight="bold"))
-            for x in DATABASE[k]: books_col.controls.append(create_card(x['title'], x['link'], ft.icons.BOOK, "orange"))
+            for item in DATABASE[k]:
+                books_col.controls.append(create_card(item['title'], item['link'], ft.icons.BOOK, "orange"))
 
-    # Papers View
+    # -- PAPERS CONTENT --
     papers_col = ft.Column(scroll="auto", padding=15)
     for k in ["papers_2024", "papers_2023", "papers_2022"]:
         if k in DATABASE:
             papers_col.controls.append(ft.Text(k.replace("papers_", "YEAR ").upper(), color="cyan", weight="bold"))
-            for x in DATABASE[k]: papers_col.controls.append(create_card(x['title'], x['link'], ft.icons.DESCRIPTION, "cyan"))
+            for item in DATABASE[k]:
+                papers_col.controls.append(create_card(item['title'], item['link'], ft.icons.DESCRIPTION, "cyan"))
 
-    # College View
+    # -- COLLEGE CONTENT --
     college_col = ft.Column(scroll="auto", padding=15)
     college_col.controls.append(ft.Text("ADMISSION TRACKER", color="green", weight="bold"))
     for x in DATABASE["colleges"]: college_col.controls.append(create_college_card(x))
 
-    # Syllabus View
+    # -- SYLLABUS CONTENT --
     syllabus_col = ft.Column(scroll="auto", padding=15)
     syllabus_col.controls.append(ft.Text("LATEST SYLLABUS", color="purple", weight="bold"))
     for x in DATABASE["syllabus_2025"]: syllabus_col.controls.append(create_card(x['title'], x['link'], ft.icons.LIST_ALT, "purple"))
 
-    # --- 6. SWAP TO MAIN UI ---
-    page.clean()
-
+    # --- 5. STRUCTURE ---
+    
+    # Body Container (Swaps content)
     body = ft.Container(content=books_col, expand=True)
 
     def on_nav(e):
@@ -227,7 +222,17 @@ def main(page: ft.Page):
         border=ft.border.only(bottom=ft.border.BorderSide(1, "#222"))
     )
 
-    page.add(header, body, nav_bar)
+    # Notification
+    banner = ft.Container(
+        content=ft.Row([ft.Icon(ft.icons.CAMPAIGN, color="black"), ft.Text("News: 2025 Routine Released!", color="black", weight="bold")]),
+        bgcolor="amber", padding=10, border_radius=8, margin=ft.margin.all(15)
+    )
+
+    # Add to Page (Order matters)
+    # Header -> Banner -> Body -> Nav
+    layout = ft.Column([header, banner, body], expand=True, spacing=0)
+    
+    page.add(layout, nav_bar)
 
 if __name__ == "__main__":
     ft.app(target=main)
